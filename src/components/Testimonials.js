064da169cf4e74dd6feb5588520f10d98e85ff99
Imran Basha <ibasha66@gmail.com>
2021-09-11 19:58:57 +0530

    Removing unwanted string interpolation

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -65,12 +65,12 @@ const Testimonials = () => {
               ))}
             </div>
             <div className="pagination-buttons">
-              <a href={activeTestimonial.id === 0 ? `#slide-${10}` : `#slide-${activeTestimonial.id}`} className="arrow left-arrow cursor-pointer" onClick={prevSlide}>
+              <a href={activeTestimonial.id === 0 ? "#slide-10" : `#slide-${activeTestimonial.id}`} className="arrow left-arrow cursor-pointer" onClick={prevSlide}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
                 </svg>
               </a>
-              <a href={activeTestimonial.id === 11 ? `#slide-${1}` : `#slide-${activeTestimonial.id}`} className="arrow right-arrow cursor-pointer" onClick={nextSlide}>
+              <a href={activeTestimonial.id === 11 ? "#slide-1" : `#slide-${activeTestimonial.id}`} className="arrow right-arrow cursor-pointer" onClick={nextSlide}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
                 </svg>
